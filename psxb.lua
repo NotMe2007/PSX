97dab80b8e8478fdc82a82e05ae0207c376c8919
SeneX <erighdaseos@gmail.com>
2025-10-28 00:35:39 +0200

    Update psxb.lua

--- psxb.lua
+++ psxb.lua
@@ -2,7 +2,7 @@
 -- Pet Simulator X GUI
 
 -- For Preston:
--- Sorry for any incovenience I don't make any malicous script like mail/bank stealers, trade scam and this shit, just auto-farm and QoL scripts, feel free to use this repo to fix any vulnerability on your game
+-- Sorry for any inconvenience. I don't make any malicious scripts like mail/bank stealers, trade scam, and this shit, just auto-farm and QoL scripts, feel free to use this repo to fix any vulnerability on your game
 --]]
 
 
@@ -12,7 +12,7 @@
 --[[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NotMe2007/PSX/refs/heads/main/psxb.lua"))()
 -- TODO LIST:
--- • Huge notifier on Discord Webhook (its ez but I'm lazy)
+-- • Huge notifier on Discord Webhook (it's ez but I'm lazy)
 -- • Auto quest
 -- • Improve Bank Index with "Auto buy storage upgrades" (+ withdraw needed diamonds from bank)
 --]]
@@ -20,11 +20,11 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/NotMe2007/PSX/refs/he
 
 -- Important Variables
 local SCRIPT_NAME = "Rafa PSX GUI"
-local SCRIPT_VERSION = "v0.3" -- Hey rafa remember to change it before updating lmao
+local SCRIPT_VERSION = "v0.3" -- Hey Rafa, remember to change it before updating lmao
 
 -- Version Control & Auto-Disable System
 if _G.PSX_LOADED and _G.PSX_VERSION then
-    -- Disable all features from old version
+    -- Disable all features from the old version
     if _G.PSX_DISABLE_FUNC then
         _G.PSX_DISABLE_FUNC()
 	end
@@ -42,7 +42,7 @@ end
 _G.PSX_VERSION = SCRIPT_VERSION
 _G.PSX_LOADED = true
 
--- Create disable function for this version
+-- Create a disable function for this version
 _G.PSX_DISABLE_FUNC = function()
     pcall(function()
         -- Disable all toggles
